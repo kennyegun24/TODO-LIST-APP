7d65a255ce12ac8d3c61b265d5b156d01ed3dd15
kenny Egun <kennyegun240@gmail.com>
2022-09-30 01:28:23 +0100

    Add more functionalities to App

--- src/index.js
+++ src/index.js
@@ -1,33 +1,79 @@
 import './style.css';
 
 const form = document.querySelector('form');
+const input = document.querySelector('.input');
 const todoContainer = document.querySelector('.todoContainer');
 
-const lists = [{
-  description: 'Start being me',
-  bool: 'false',
-  index: '1',
-},
-{
-  description: 'Just be happy',
-  bool: 'false',
-  index: '2',
-}, {
-  description: 'I am kenny',
-  bool: 'false',
-  index: '3',
-}];
+let todo;
+let todos = JSON.parse(localStorage.getItem('todos')) || [];
+const store = () =>{
+  todo = {
+    Description: input.value,
+    id:todos.length,
+    completed: false,
+    }
+  todos.push(todo);
+  localStorage.setItem('todos', JSON.stringify(todos));
+  }
 
-lists.forEach((task) => {
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const ul = document.createElement('ul');
-    const hr = document.createElement('hr');
-    ul.innerHTML = `
-    <li>
-    <input class="checkBox" type="checkbox">${task.description}<i class="fa-solid fa-ellipsis-vertical dots"></i>
-    </li>
-    `;
-    todoContainer.append(ul, hr);
+const clear = () => {
+  input.value = ''
+}
+
+const removeBook = (id) => {
+  todos = todos.filter((books) => books.id !== id);
+  todos.forEach((todo, id) => {
+    todo.id = id
+  })
+  localStorage.setItem('todos', JSON.stringify(todos));{
+  }
+}
+
+const addTask = (todo) => {
+    const ul = document.createElement('div');
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    checkBox.classList.add('checkBox')
+    const newInp = document.createElement('input')
+    newInp.type = 'text'
+    newInp.classList.add('newInput')
+    newInp.value = todo.Description
+    const icon = document.createElement('i')
+    icon.classList.add('fa-solid')
+    icon.classList.add('fa-ellipsis-vertical')
+    icon.classList.add('dots')
+    const hr = document.createElement('hr')
+    ul.append(checkBox, newInp, icon, hr)
+    todoContainer.append(ul);
+    icon.addEventListener('click', () => {
+    icon.parentElement.remove()
+    removeBook(todo.id)
+  })
+};
+todos.forEach(addTask)
+
+const editTodoList = () => {
+const editInput = document.querySelectorAll('.newInput');
+editInput.forEach((edits, indexy) => {
+  edits.addEventListener('change', () => {
+    todos.forEach((todo, index) => {
+      if (indexy === index) {
+        todo.Description = edits.value;
+        localStorage.setItem('todos', JSON.stringify(todos));
+      };
+    });
   });
 });
+};
+editTodoList()
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  if (input.value !== '') {
+    store();
+    addTask(todo)
+    clear()
+  } else {
+    alert('Please enter a title and author');
+  }
+})
